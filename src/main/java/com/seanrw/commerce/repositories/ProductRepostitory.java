eb80ac849d4396cdba78b45b877ae4ebd64f30a2
sean-rw <seanrwhitley@gmail.com>
2023-02-16 13:00:56 -0500

    Defining @Query for ProductRepository

--- src/main/java/com/seanrw/commerce/repositories/ProductRepostitory.java
+++ src/main/java/com/seanrw/commerce/repositories/ProductRepostitory.java
@@ -3,11 +3,13 @@ package com.seanrw.commerce.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.seanrw.commerce.models.Product;
 
 public interface ProductRepostitory extends JpaRepository<Product, Long> {
     
+    @Query(value = "SELECT * FROM products WHERE name = ?1", nativeQuery = true)
     List<Product> findByName(String name);
 
 }
