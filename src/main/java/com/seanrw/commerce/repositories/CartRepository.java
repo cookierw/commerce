6e7d40f1a004cd87f5144b2e905a04f0d154d154
sean-rw <seanrwhitley@gmail.com>
2023-02-16 14:21:23 -0500

    Note for later

--- src/main/java/com/seanrw/commerce/repositories/CartRepository.java
+++ src/main/java/com/seanrw/commerce/repositories/CartRepository.java
@@ -1,12 +1,13 @@
 package com.seanrw.commerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.seanrw.commerce.models.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     
-    // Custom @Query
+    // @Query(value = "SELECT * FROM carts WHERE status = 0", nativeQuery = true)
     // Cart findActiveCart();
 
 }
