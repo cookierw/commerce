e6f0c48c53d429814d982ef953289d24de0dd53c
sean-rw <seanrwhitley@gmail.com>
2023-02-16 14:21:01 -0500

    Replace Spring Security with JsonWebToken library, as we only need JWT generation

---
+++ src/main/java/com/seanrw/commerce/services/TokenService.java
@@ -0,0 +1,48 @@
+package com.seanrw.commerce.services;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.seanrw.commerce.config.JwtConfig;
+import com.seanrw.commerce.dtos.responses.Principal;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+
+public class TokenService {
+    
+    private JwtConfig jwtConfig;
+
+    public TokenService(@Autowired JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    public String tokenize(Principal subject) {
+        long now = System.currentTimeMillis();
+
+        JwtBuilder builder = Jwts.builder()
+                                .setId(subject.getId().toString())
+                                .setIssuer("commerce")
+                                .setIssuedAt(new Date(now))
+                                .setExpiration(new Date(now + jwtConfig.getExpiration()))
+                                .setSubject(subject.getUsername())
+                                .claim("role", subject.getRole())
+                                .signWith(jwtConfig.getSigAlg(), jwtConfig.getSigningKey());
+        
+        return builder.compact();
+    }
+
+    public Principal extractRequesterDetails(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtConfig.getSigningKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+            return new Principal(Long.parseLong(claims.getId()), claims.getSubject(), claims.get("role", String.class));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
