6b9d2f332dd9496164379ccaa137a0faa2b4fdd1
sean-rw <seanrwhitley@gmail.com>
2023-02-15 22:24:43 -0500

    Oops

--- src/main/java/com/seanrw/commerce/repositories/UserRepository.java
+++ src/main/java/com/seanrw/commerce/repositories/UserRepository.java
@@ -18,6 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT (username) FROM users WHERE username = ?1", nativeQuery = true)
     List<String> findAllUsernames(String username);
 
-    @Query(value = "SELECT * FROM users WHERE username = ?1 AND password = ?2")
+    @Query(value = "SELECT * FROM users WHERE username = ?1 AND password = ?2", nativeQuery = true)
     User findByUsernameAndEmail(String username, String password);
 }
