7b96d5b26746aded8974d0b09ac409808ef05e6e
sean-rw <seanrwhitley@gmail.com>
2023-02-20 19:58:35 -0500

    Base AuthController impl

--- src/main/java/com/seanrw/commerce/controllers/AuthController.java
+++ src/main/java/com/seanrw/commerce/controllers/AuthController.java
@@ -1,11 +1,25 @@
 package com.seanrw.commerce.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seanrw.commerce.services.TokenService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
     
 }
