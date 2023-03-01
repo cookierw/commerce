d3e33602bcebe7ed53100e2a3a1b59b5099013b1
sean-rw <seanrwhitley@gmail.com>
2023-03-01 14:24:38 -0500

    Unneeded 'this'

--- src/main/java/com/seanrw/commerce/services/TokenService.java
+++ src/main/java/com/seanrw/commerce/services/TokenService.java
@@ -32,7 +32,7 @@ public class TokenService {
                 .subject(authentication.getName())
                 .claim("scope", scope)
                 .build();
-        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
 }
