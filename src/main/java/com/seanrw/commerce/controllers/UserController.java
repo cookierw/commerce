63d328c9dc804a1e6785b49cd4772ce8d7d14961
sean-rw <seanrwhitley@gmail.com>
2023-03-01 14:48:04 -0500

    C(R)UD Functionality for UserController. AuthController contains the Read functionality.

--- src/main/java/com/seanrw/commerce/controllers/UserController.java
+++ src/main/java/com/seanrw/commerce/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.seanrw.commerce.controllers;
 
 import com.seanrw.commerce.dtos.requests.NewUserRequest;
 import com.seanrw.commerce.exceptions.InvalidSignupException;
+import com.seanrw.commerce.exceptions.UserNotFoundException;
 import com.seanrw.commerce.models.User;
 import com.seanrw.commerce.services.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,5 +30,29 @@ public class UserController {
         }
 
         return ResponseEntity.ok("Success"); // 200
+    }
+
+    @PutMapping("/api/user")
+    public ResponseEntity<String> update(@RequestBody User user) {
+        try {
+            userService.update(user);
+            log.info("User " + user.getUsername() + " updated successfully");
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        return ResponseEntity.ok("Success");
+    }
+
+    @DeleteMapping("/api/user")
+    public ResponseEntity<String> delete(@RequestBody User user) {
+        try {
+            userService.deleteByUser(user);
+            log.info("User " + user.getUsername() + " deleted successfully");
+        } catch (OptimisticLockingFailureException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        return ResponseEntity.ok("Success");
     }
 }
